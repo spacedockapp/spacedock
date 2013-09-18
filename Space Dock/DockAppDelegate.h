4ffeb0d3f743a90a80aff5dfec9b372fbfbb7889
Rob Tsuk <rob@tsuk.com>
2013-09-18 09:58:51 -0700

    Initial Commit

---
+++ Space Dock/DockAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  DockAppDelegate.h
+//  Space Dock
+//
+//  Created by Rob Tsuk on 9/18/13.
+//  Copyright (c) 2013 Rob Tsuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DockAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Space Dock/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Space Dock
+//
+//  Created by Rob Tsuk on 9/18/13.
+//  Copyright (c) 2013 Rob Tsuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
