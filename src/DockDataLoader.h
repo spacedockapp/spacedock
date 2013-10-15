8853a4637bf9a4de0f541fbf1adfb80597d979c8
Rob Tsuk <rob@tsuk.com>
2013-10-14 20:48:02 -0700

    Revert "Merge branch 'refs/heads/master' into rwt-ios"
    This reverts commit 9044b76afa7e643b9a934896a65e28b372ca6a94, reversing
    changes made to 307c13615a1d08b460d9e9fdbe665fb9842453c2.

--- src/DockDataLoader.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface DockDataLoader : NSObject
-@property (readonly, weak, nonatomic) NSManagedObjectContext* managedObjectContext;
--(id)initWithContext:(NSManagedObjectContext*)context;
--(BOOL)loadData:(NSError**)error;
--(NSSet*)validateSpecials;
-@end
--- src/DockUtils.h
+++ src/DockUtils.h
@@ -1,7 +1,4 @@
 #import <Foundation/Foundation.h>
 
-#if MAC
 NSAttributedString* coloredString(NSString* text, NSColor* color, NSColor* backColor);
-#endif
-
 NSSet* allAttributes(NSManagedObjectContext* context, NSString* entityName, NSString* attributeName);
