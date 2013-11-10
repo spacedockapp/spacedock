5d604065cd853a7021cbae7298a70d0470c905f2
Rob Tsuk <rob@tsuk.com>
2013-11-09 23:07:02 -0800

    Don't use untitled as a default squad name.

--- ios/DockSquadsListController.m
+++ ios/DockSquadsListController.m
@@ -149,15 +149,6 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -182,22 +173,6 @@
     }   
 }
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Navigation
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -215,9 +190,8 @@
     } else if ([identifier isEqualToString:@"NameNewSquad"]) {
         DockEditValueController* editValue = (DockEditValueController*)destination;
         editValue.valueName = @"Name";
-        editValue.initialValue = @"Untitled";
+        editValue.initialValue = @"";
         editValue.onSave = ^(NSString* newValue) {
-            NSLog(@"create new squad %@", newValue);
             NSEntityDescription* entity = [NSEntityDescription entityForName: @"Squad"
                                                       inManagedObjectContext: _managedObjectContext];
             DockSquad* newSquad = [[DockSquad alloc] initWithEntity: entity
