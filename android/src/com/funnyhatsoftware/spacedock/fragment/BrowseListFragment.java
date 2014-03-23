881ec83e3efe4547dd47638b9323c4bb347fb8f2
Rob Tsuk <rob@tsuk.com>
2014-03-23 09:37:34 -0700

    Have the items in the Browse screen in the same order and same capitalization as they are in other versions of Space Dock.

--- android/src/com/funnyhatsoftware/spacedock/fragment/BrowseListFragment.java
+++ android/src/com/funnyhatsoftware/spacedock/fragment/BrowseListFragment.java
@@ -1,15 +1,22 @@
 package com.funnyhatsoftware.spacedock.fragment;
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.funnyhatsoftware.spacedock.holder.ItemHolderFactory;
-
-import java.util.ArrayList;
+import com.funnyhatsoftware.spacedock.R;
+import com.funnyhatsoftware.spacedock.holder.CaptainHolder;
+import com.funnyhatsoftware.spacedock.holder.FlagshipHolder;
+import com.funnyhatsoftware.spacedock.holder.ResourceHolder;
+import com.funnyhatsoftware.spacedock.holder.SetHolder;
+import com.funnyhatsoftware.spacedock.holder.ShipHolder;
+import com.funnyhatsoftware.spacedock.holder.UpgradeHolder;
+import com.funnyhatsoftware.spacedock.holder.WeaponHolder;
 
 public class BrowseListFragment extends ListFragment {
     public interface BrowseTypeSelectionListener {
@@ -17,13 +24,26 @@ public class BrowseListFragment extends ListFragment {
     }
 
     ArrayAdapter<String> mAdapter;
+    
+    /* This list must be kept in sync with R.array.browse_items_list */
+    static final String[] TYPE_MAP = {
+        ShipHolder.TYPE_STRING,
+        CaptainHolder.TYPE_STRING,
+        UpgradeHolder.TYPE_STRING_CREW,
+        UpgradeHolder.TYPE_STRING_TALENT,
+        UpgradeHolder.TYPE_STRING_TECH,
+        WeaponHolder.TYPE_STRING,
+        ResourceHolder.TYPE_STRING,
+        FlagshipHolder.TYPE_STRING,
+        SetHolder.TYPE_STRING
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayList<String> targets = new ArrayList<String>();
-        targets.addAll(ItemHolderFactory.getFactoryTypes());
+        String[] a = getResources().getStringArray(R.array.browse_items_list);
+        List<String> targets = Arrays.asList(a);
 
         mAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_activated_1,
@@ -34,7 +54,7 @@ public class BrowseListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        String targetType = mAdapter.getItem(position);
+        String targetType = TYPE_MAP[position];
         ((BrowseTypeSelectionListener) getActivity()).onBrowseTypeSelected(targetType);
     }
 }
