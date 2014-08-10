e07ea681cb7e3c1fe9f9febc6792bb24fac21668
Rob Tsuk <rob@tsuk.com>
2014-08-10 07:06:02 -0700

    Set up project for MetaDock test driven development.

---
+++ MetaDock/MetaDock/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MetaDock
+//
+//  Created by Rob Tsuk on 8/10/14.
+//  Copyright (c) 2014 Rob Tsuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ MetaDock/MetaDockTests/MetaDockTests.m
@@ -0,0 +1,24 @@
+#import <XCTest/XCTest.h>
+
+@interface MetaDockTests : XCTestCase
+
+@end
+
+@implementation MetaDockTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
