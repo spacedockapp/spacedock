30f9bfafbd080f6ecbf4013bd1c6cc7678fb4b23
Rob Tsuk <rob@tsuk.com>
2014-08-10 10:23:44 -0700

    Add a game system object.

--- MetaDock/MetaDock/DockUniverse.m
+++ MetaDock/MetaDock/DockUniverse.m
@@ -1,13 +1,32 @@
-//
-//  DockUniverse.m
-//  MetaDock
-//
-//  Created by Rob Tsuk on 8/10/14.
-//  Copyright (c) 2014 Rob Tsuk. All rights reserved.
-//
-
 #import "DockUniverse.h"
 
+#import "DockGameSystem.h"
+
 @implementation DockUniverse
+
+-(id)initWithDataStorePath:(NSString*)dataStore templatesPath:(NSString*)templatesPath
+{
+    self = [super init];
+    if (self != nil) {
+        [self setupGameSystems: templatesPath];
+    }
+    return self;
+}
+
+#pragma mark - Game Systems
+
+-(void)setupGameSystems:(NSString*)templatesPath
+{
+    NSFileManager* fm = [NSFileManager defaultManager];
+    NSError* error;
+    NSArray* gameSystemFolders = [fm contentsOfDirectoryAtPath: templatesPath error: &error];
+    NSMutableArray* gameSystems = [[NSMutableArray alloc] initWithCapacity: gameSystemFolders.count];
+    for (NSString* folderName in gameSystemFolders) {
+        NSString* gameSystemPath = [templatesPath stringByAppendingPathComponent: folderName];
+        DockGameSystem* gameSystem = [[DockGameSystem alloc] initWithPath: gameSystemPath];
+        [gameSystems addObject: gameSystem];
+    }
+    _gameSystems = [NSSet setWithArray: gameSystems];
+}
 
 @end
