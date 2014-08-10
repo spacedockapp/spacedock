86276e8d4ae1bf687d92cc476a7b847a1b677ded
Rob Tsuk <rob@tsuk.com>
2014-08-10 09:09:13 -0700

    Use the recommended pattern for class initialization.

---
+++ MetaDock/MetaDock/DockUniverse.h
@@ -0,0 +1,13 @@
+//
+//  DockUniverse.h
+//  MetaDock
+//
+//  Created by Rob Tsuk on 8/10/14.
+//  Copyright (c) 2014 Rob Tsuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DockUniverse : NSObject
+
+@end
